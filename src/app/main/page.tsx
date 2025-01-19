7fa784b4018abf833ab5037c00e807db6e3aabce
Yandra Muslim <yandrayt1@gmail.com>
2025-01-19 10:32:22 +0700

    update:1

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 import React from "react";
 
 const MainPage = () => {
+  const isOnline =
+    typeof window !== "undefined" ? window.navigator.onLine : false;
   return (
     <div className="flex items-center flex-col justify-center min-h-screen">
       <div>
         Status:{" "}
-        {window.navigator.onLine ? (
+        {isOnline ? (
           <span className="bg-green-600 rounded-lg p-0.5">online</span>
         ) : (
           <span className="bg-red-600 rounded-lg p-0.5">online</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const isOnline =
+    typeof window !== "undefined" ? window.navigator.onLine : false;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -26,7 +28,7 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
           <li>
             Status:{" "}
-            {window.navigator.onLine ? (
+            {isOnline ? (
               <span className="bg-green-600 rounded-lg p-0.5">online</span>
             ) : (
               <span className="bg-red-600 rounded-lg p-0.5">online</span>
